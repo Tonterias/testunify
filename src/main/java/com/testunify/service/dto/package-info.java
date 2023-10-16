5a27756606310e21a1c5c2bbb3c910782c3ffb92
Mike <miguel7suarez7@gmail.com>
2023-10-16 19:43:45 +0200

    Initial version of testunify generated by generator-jhipster@7.9.4

---
+++ src/main/java/com/testunify/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.testunify.service.dto;
---
+++ src/test/java/com/testunify/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.testunify;
+
+import com.testunify.TestunifyApp;
+import com.testunify.config.AsyncSyncConfiguration;
+import com.testunify.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { TestunifyApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
